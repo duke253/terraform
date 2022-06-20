a82e7158a5b277806064ea267cf683c80b78f0f8
Grigoriy Ilduykov <grigoriy.ilduykov@yandex.ru>
2022-06-20 16:55:39 +0300

    created

---
+++ data.tf
@@ -0,0 +1,3 @@
+data "yandex_vpc_subnet" "k8s_subnet" {
+  subnet_id = "e9bguedmg6sln5sf6cag"
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "YC_REGION" {
+  type    = string
+  default = "ru-central1-a"
+}
+
+variable "YC_TOKEN" {
+  type        = string
+  description = "..."
+}
+
+variable "YC_CLOUD_ID" {
+  type        = string
+  description = "..."
+}
+
+variable "YC_FOLDER_ID" {
+  type        = string
+  description = "..."
+}
+
+variable "SSH_KEY_OPEN" {
+  type        = string
+  description = "..."
+}
